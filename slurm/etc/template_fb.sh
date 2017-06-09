ecc05f63377303892484ee321a8bd7f8ab1513e2
Francisco Ortuno Guzman <franciscoortuno@Franciscos-MacBook-Air.local>
2017-06-09 14:36:51 -0500

    feat(slurm): init slurm freebayes

---
+++ slurm/etc/template_fb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=XX_THREAD_COUNT_XX
+#SBATCH --ntasks=1
+#SBATCH --mem=XX_MEM_XX
+#SBATCH --workdir="/mnt/SCRATCH/"
+
+function cleanup (){
+    echo "cleanup tmp data";
+    sudo rm -rf $basedir;
+}
+
+input_list="XX_INPUTLIST_XX"
+output_id="XX_OUTPUT_ID_XX"
+project="XX_PROJECT_XX"
+s3_profile="XX_S3PROFILE_XX"
+s3_endpoint="XX_S3ENDPOINT_XX"
+
+thread_count="XX_THREAD_COUNT_XX"
+java_heap="XX_JAVAHEAP_XX"
+
+basedir=`sudo mktemp -d genomel.fb.XXXXXXXXXX -p /mnt/SCRATCH/`
+refdir="XX_REFDIR_XX"
+s3dir="XX_S3DIR_XX"
+intervals="XX_INTERVALS_XX"
+chunk="XX_BLOCK_XX"
+
+repository="git@github.com:uc-cdis/cwl.git"
+sudo chown ubuntu:ubuntu $basedir
+
+cd $basedir
+
+sudo git clone -b feat/slurm $repository genomel_cwl
+sudo chown ubuntu:ubuntu -R genomel_cwl
+
+trap cleanup EXIT
+
+/home/ubuntu/.virtualenvs/p2/bin/python genomel_cwl/slurm/genomel_fb_pipeline.py \
+--input_list $input_list \
+--output_id $output_id \
+--project $project \
+--s3_profile $s3_profile \
+--s3_endpoint $s3_endpoint \
+--basedir $basedir \
+--refdir $refdir \
+--cwl $basedir/genomel_cwl/tools/variant_detection/freebayes/freebayes.cwl.cwl.yaml \
+--s3dir $s3dir \
+--thread_count $thread_count \
+--java_heap $java_heap \
+--chunk $chunk \
+--intervals $intervals
---
+++ slurm/genomel_fb_pipeline.py
@@ -0,0 +1,218 @@
+'''
+Main wrapper script for FreeBayes workflow
+'''
+import os
+import time
+import argparse
+import logging
+import sys
+import uuid
+import tempfile
+import utils.s3
+import utils.pipeline
+import datetime
+import socket
+import glob
+import json
+
+import postgres.status
+import postgres.utils
+import postgres.mixins
+
+from sqlalchemy.exc import NoSuchTableError
+
+def is_nat(x):
+    '''
+    Checks that a value is a natural number.
+    '''
+    if int(x) >= 0:
+        return int(x)
+    raise argparse.ArgumentTypeError('%s must be positive' % x)
+
+def get_args():
+    '''
+    Loads the parser
+    '''
+    # Main parser
+    parser = argparse.ArgumentParser(description="Genomel Recalibration Pipeline")
+    # Args
+    required = parser.add_argument_group("Required input parameters")
+    # Metadata from input table
+    required.add_argument("--input_list", default=None, help="Path to pre-staged files")
+    required.add_argument("--project", default=None, help="PROJECT, PDC project name.")
+    required.add_argument("--s3_profile", required=True, help="S3 profile name for project tenant.")
+    required.add_argument("--s3_endpoint", required=True, help="S3 endpoint url for project tenant.")
+    # Parameters for pipeline
+    required.add_argument("--basedir", default="/mnt/SCRATCH/", help="Base directory for computations.")
+    required.add_argument("--refdir", required=True, help="Path to reference directory.")
+    required.add_argument("--cwl", required=True, help="Path to Genomel Recalibration CWL workflow yaml.")
+    required.add_argument("--s3dir", default="s3://", help="S3 dir for uploading output files.")
+    required.add_argument('--output_id', required=True, help='UUID for the output')
+
+
+    required.add_argument('--java_heap', required=True, help='Java heap memory limit.')
+    required.add_argument('--thread_count', type=is_nat, default=8, help='Threads count.')
+
+    required.add_argument('--chunk', type=is_nat, default=30, help='Chunk order')
+    required.add_argument('--intervals', type=is_nat, default=30, help='Number of total regions')
+
+
+    return parser.parse_args()
+
+
+def run_pipeline(args, statusclass, metricsclass):
+    '''
+    Executes the CWL pipeline and record status/metrics tables
+    '''
+    if not os.path.isdir(args.basedir):
+        raise Exception("Could not find path to base directory: %s" %args.basedir)
+    
+    # Get hostname
+    hostname = socket.gethostname()
+    # Get datetime start
+    datetime_start = str(datetime.datetime.now())
+    
+    # Create directory structure
+    jobdir = tempfile.mkdtemp(prefix="fb_%s_" % str(args.output_id), dir=args.basedir)
+    workdir = tempfile.mkdtemp(prefix="workdir_", dir=jobdir)
+    inputdir = tempfile.mkdtemp(prefix="input_", dir=jobdir)
+    resultdir = tempfile.mkdtemp(prefix="result_", dir=jobdir)
+    refdir = args.refdir
+    
+    # Setup logger
+    log_file = os.path.join(resultdir, "%s.genomel.fb.cwl.log" % str(args.output_id))
+    logger = utils.pipeline.setup_logging(logging.INFO, str(args.output_id), log_file)
+    
+    # Logging inputs
+    logger.info("hostname: %s" % (hostname))
+    logger.info("input_id: %s" % (args.input_list))
+    logger.info("project: %s" % (args.project))
+    logger.info("datetime_start: %s" % (datetime_start))
+    
+    # Setup start point
+    cwl_start = time.time()
+    download_time = cwl_start - cwl_start
+    
+    # Getting refs
+    logger.info("getting resources")
+    reference_data        = utils.pipeline.load_reference_json('etc/reference.json')
+    reference_fasta_path  = os.path.join(refdir, reference_data["reference_fasta"])   
+    reference_intervals   = os.path.join(refdir, reference_data["reference_intervals"])     
+    postgres_config       = os.path.join(refdir, reference_data["pg_config"])
+
+    # Calculating region according to chunk
+    logger.info("creating interval file")
+    chunked_intervals = utils.pipeline.get_interval_region(reference_intervals, workdir, args.chunk, args.intervals)
+    logger.info("interval file: %s" % chunked_intervals)
+
+    # Logging pipeline info
+    cwl_version    = reference_data["cwl_version"]
+    docker_version = reference_data["docker_version"]
+    logger.info("cwl_version: %s" % (cwl_version))
+    logger.info("docker_version: %s" % (docker_version))
+    
+    # Define output
+    output_gvcf = os.path.join(workdir, args.output_id + '.vcf.gz')
+
+    # Get input files
+    file_array = []
+    input_id = []
+    with open(args.input_list, 'r') as filelist:
+        for line in filelist:
+            columns = line.strip('\n').split('\t')
+            input_id.append(columns[0])
+            file_array.append({"class": "File", "path": columns[1]})
+    
+    # Create input json
+    input_json_file = os.path.join(resultdir, '{0}.genomel.fb.inputs.json'.format(str(args.output_id)))
+    input_json_data = {
+      "input_bam_path": file_array,
+      "reference_fasta_path": {"class": "File", "path": reference_fasta_path},
+      "interval_file_path": {"class": "File", "path": chunked_intervals}, 
+      "output_vcf_name": os.path.basename(output_gvcf)      
+    }
+
+    with open(input_json_file, 'wt') as o:
+        json.dump(input_json_data, o, indent=4)
+    logger.info("Preparing input json: %s" % (input_json_file))
+    
+    # Run CWL
+    os.chdir(workdir)
+    logger.info('Running CWL workflow')
+    cmd = ['/usr/bin/time', '-v',
+           '/home/ubuntu/.virtualenvs/p2/bin/cwltool',
+           "--debug",
+           "--tmpdir-prefix", inputdir,
+           "--tmp-outdir-prefix", workdir,
+           args.cwl,
+           input_json_file]
+    cwl_exit = utils.pipeline.run_command(cmd, logger)
+    cwl_failure = False
+    if cwl_exit:
+        cwl_failure = True
+
+    # Get md5 and file size
+    md5 = utils.pipeline.get_md5(output_gvcf)
+    file_size = utils.pipeline.get_file_size(output_gvcf)
+    
+    # Upload output
+    upload_start = time.time()
+    upload_dir_location = os.path.join(args.s3dir, str(args.output_id))
+    upload_gvcf_location = os.path.join(upload_dir_location, os.path.basename(output_gvcf))    
+    logger.info("Uploading workflow output to %s" % (upload_gvcf_location))
+    upload_exit  = utils.s3.aws_s3_put(logger, upload_gvcf_location, output_gvcf, args.s3_profile, args.s3_endpoint, recursive=False)
+
+    # Establish connection with database
+    engine = postgres.utils.get_db_engine(postgres_config)
+    
+    # Calculate times
+    cwl_end = time.time()
+    upload_time = cwl_end - upload_start
+    cwl_elapsed = cwl_end - cwl_start
+    datetime_end = str(datetime.datetime.now())
+    logger.info("datetime_end: %s" % (datetime_end))
+    
+    # Get status info
+    logger.info("Get status/metrics info")
+    status, loc = postgres.status.get_status(upload_exit, cwl_exit, upload_gvcf_location, upload_dir_location, logger)
+    
+    # Get metrics info
+    time_metrics = utils.pipeline.get_time_metrics(log_file)
+    
+    # Set status table
+    logger.info("Updating status")
+    postgres.utils.add_pipeline_status(engine, args.output_id, input_id, args.output_id,
+                                       status, loc, datetime_start, datetime_end,
+                                       md5, file_size, hostname, cwl_version, docker_version, statusclass)
+    # Set metrics table
+    logger.info("Updating metrics")
+    postgres.utils.add_pipeline_metrics(engine, args.output_id, input_id, download_time,
+                                        upload_time, args.thread_count, cwl_elapsed,
+                                        time_metrics['system_time'],
+                                        time_metrics['user_time'],
+                                        time_metrics['wall_clock'],
+                                        time_metrics['percent_of_cpu'],
+                                        time_metrics['maximum_resident_set_size'],
+                                        status, metricsclass)
+    
+    # Remove job directories, upload final log file
+    logger.info("Uploading main log file")
+    utils.s3.aws_s3_put(logger, upload_dir_location + '/' + os.path.basename(log_file), log_file, args.s3_profile, args.s3_endpoint, recursive=False)
+    utils.pipeline.remove_dir(jobdir)
+
+
+
+if __name__ == '__main__':
+    
+    # Get args
+    args = get_args()
+    project = args.project.lower()
+    
+    # Setup postgres classes for tables
+    class CallerStatus(postgres.mixins.StatusTypeMixin, postgres.utils.Base):
+        __tablename__ = project + '_fb_cwl_status'
+    class CallerMetrics(postgres.mixins.MetricsTypeMixin, postgres.utils.Base):
+        __tablename__ = project + '_fb_cwl_metrics'
+    
+    # Run pipeline
+    run_pipeline(args, CallerStatus, CallerMetrics)
