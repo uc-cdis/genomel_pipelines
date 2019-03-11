9c791a0f0e1e4cd63912e4a4c763ea0a0bb92c08
Shenglai <sli6@uchicago.edu>
2019-03-11 16:54:17 -0500

    fix(slurm): change glusterfs to cephfs

--- genomel/slurm/etc/cohort_genotyping.sh
+++ genomel/slurm/etc/cohort_genotyping.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --workdir="/mnt/glusterfs/cromwell_workdir"
+#SBATCH --workdir="/mnt/cephfs/cromwell_workdir"
 
-basedir="/mnt/glusterfs/cromwell_workdir"
+basedir="/mnt/cephfs/cromwell_workdir"
 project="{PROJECT}"
 batch_id="{BATCH}"
 job_uuid="{JOB_UUID}"
 input_table="{INPUT_TABLE}"
 psql_conf="/mnt/SCRATCH/reference/postgres_config"
-gvcf_files_manifest="/mnt/glusterfs/cromwell_workdir/gvcf_local_path.list"
+gvcf_files_manifest="/mnt/cephfs/cromwell_workdir/gvcf_local_path.list"
 gatk4_genotyping_thread_count="20"
-number_of_chunks_for_gatk="50"
-bam_files_manifest="/mnt/glusterfs/cromwell_workdir/bam_local_path.list"
+number_of_chunks_for_gatk="100"
+bam_files_manifest="/mnt/cephfs/cromwell_workdir/bam_local_path.list"
 freebayes_thread_count="1"
-number_of_chunks_for_freebayes="50"
+number_of_chunks_for_freebayes="100"
 upload_s3_bucket="s3://genomel/cohort_genotyping_output/"
-cromwell_jar_path="/mnt/glusterfs/cromwell_workdir/cromwell-36.jar"
+cromwell_jar_path="/mnt/cephfs/cromwell_workdir/cromwell-38.jar"
 repository="git@github.com:uc-cdis/genomel_pipelines.git"
 
 cd $basedir
 
-sudo git clone $repository genomel_pipelines
+sudo git clone -b feat/prod $repository genomel_pipelines
 sudo chown ubuntu:ubuntu -R genomel_pipelines
 
 /home/ubuntu/.virtualenvs/p2/bin/python \
