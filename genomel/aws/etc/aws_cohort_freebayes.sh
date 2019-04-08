a837e70897f4d99b40b2a1b72bf10d2fb1549931
Shenglai <sli6@uchicago.edu>
2019-04-08 10:17:34 -0500

    chore(aws): update minor changes for aws prod

--- genomel/aws/etc/aws_cohort_freebayes.sh
+++ genomel/aws/etc/aws_cohort_freebayes.sh
@@ -5,15 +5,13 @@ project="genomel_prod"
 batch_id="aws_freebayes_01"
 job_uuid="13189d29-db47-4830-a895-f2b907a39167"
 bam_files_manifest="/data/cromwell_workdir/bam_local_path.list"
-freebayes_thread_count=""
-number_of_chunks_for_freebayes=""
+freebayes_thread_count="64"
+number_of_chunks_for_freebayes="64"
 upload_s3_bucket="s3://genomel/cohort_genotyping_output/"
 cromwell_jar_path="/data/cromwell_workdir/cromwell-36.jar"
 repository="git@github.com:uc-cdis/genomel_pipelines.git"
 
 cd $basedir
-
-git clone feat/aws $repository genomel_pipelines
 
 /home/ubuntu/.virtualenvs/p2/bin/python \
 $basedir/genomel_pipelines/genomel/aws/aws_cohort_freebayes_runner.py \
--- genomel/cwl/tools/variant_calling/gatk3_selectvariants.cwl
+++ genomel/cwl/tools/variant_calling/gatk3_selectvariants.cwl
@@ -8,7 +8,7 @@ requirements:
   - class: InlineJavascriptRequirement
   - class: ShellCommandRequirement
   - class: DockerRequirement
-    dockerPull: registry.gitlab.com/uc-cdis/genomel-exome-variant-detection/genomel_variant_calling:2.0
+    dockerPull: registry.gitlab.com/uc-cdis/genomel-exome-variant-detection/genomel_variant_calling:4.0
 
 inputs:
   job_uuid: string
